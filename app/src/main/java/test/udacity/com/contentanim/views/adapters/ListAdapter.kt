e7fa26e925486a049c115db98f45d7fe015351b1
Bernat Gomez <bernat.gomez@mobetia.com>
2017-07-15 19:18:34 +0200

    Implement list adapter

--- app/src/main/java/test/udacity/com/contentanim/views/adapters/ListAdapter.kt
+++ app/src/main/java/test/udacity/com/contentanim/views/adapters/ListAdapter.kt
@@ -1,5 +1,29 @@
 package test.udacity.com.contentanim.views.adapters
 
+import android.support.v7.view.menu.ActionMenuItemView
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
 /**
  * Created by bernatgomez on 15/7/17.
  */
+class ListAdapter() : RecyclerView.Adapter<ListAdapter.ListHolder>() {
+    
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ListHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ListHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class ListHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
