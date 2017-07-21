4cbf97db0faa5a194ab1f34f35ec84739194e33c
Bernat Gomez <bernat.gomez@mobetia.com>
2017-07-21 17:48:15 +0200

    Add animo on detail screen

--- app/src/main/java/test/udacity/com/contentanim/views/DetailActivity.kt
+++ app/src/main/java/test/udacity/com/contentanim/views/DetailActivity.kt
@@ -1,11 +1,16 @@
 package test.udacity.com.contentanim.views
 
+
 import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
+import android.transition.Slide
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.LinearInterpolator
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
 
@@ -13,11 +18,16 @@ import test.udacity.com.contentanim.R
 import test.udacity.com.contentanim.controllers.ListController
 import test.udacity.com.contentanim.models.PhotoModel
 
+
+/**
+ * Content detail screen
+ */
 class DetailActivity : AppCompatActivity() {
 
     companion object {
         val EXTRA_PHOTO = "extra_photo"
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,31 +37,34 @@ class DetailActivity : AppCompatActivity() {
         this.configActionBar()
 
         this.setValues()
-    }
 
-    private fun getUrl(photo : PhotoModel) : Uri {
-        return Uri.parse(ListController.LIST_URL + this.resources.displayMetrics.widthPixels + ListController.LIST_ITEM_PATH + photo.id)
-    }
-
-    private fun setValues() {
-        var data : PhotoModel? = null
-
-        if (this.intent.extras.containsKey(EXTRA_PHOTO)) {
-            data = this.intent.getSerializableExtra(EXTRA_PHOTO) as PhotoModel
-
-            Picasso.with(this).load(this.getUrl(data)).into(this.photo)
-
-            this.titlee.setText(data.author)
-            this.content.setText(getString(R.string.fake_descrip))
-        }
-
+        this.configAnimations()
     }
 
     private fun configActionBar() {
         val bar : ActionBar? = this.supportActionBar
 
         bar!!.setTitle(R.string.app_name)
-        bar!!.setDisplayHomeAsUpEnabled(true)
+        bar.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    private fun setValues() {
+        var data : PhotoModel?
+
+        if (this.intent.extras.containsKey(EXTRA_PHOTO)) {
+            data = this.intent.getSerializableExtra(EXTRA_PHOTO) as PhotoModel
+
+            Picasso.with(this).load(this.getUrl(data)).into(this.photo)
+
+            this.screenTitle.text = data.author
+            this.content.text = this.getString(R.string.fake_descrip)
+        }
+
+    }
+
+    private fun getUrl(photo : PhotoModel) : Uri {
+        return Uri.parse(ListController.LIST_URL + this.resources.displayMetrics.widthPixels + ListController.LIST_ITEM_PATH + photo.id)
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
@@ -70,5 +83,23 @@ class DetailActivity : AppCompatActivity() {
                 return super.onOptionsItemSelected(item)
             }
         }
+    }
+
+    fun configAnimations() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.animateText()
+        }
+    }
+
+    private fun animateText() {
+        val slide : Slide = Slide(Gravity.BOTTOM)
+
+        slide.addTarget(R.id.content)
+
+        slide.duration = this.resources.getInteger(R.integer.anim_duration) + 0L
+
+        slide.interpolator = LinearInterpolator()
+
+        this.window.enterTransition = slide
     }
 }
